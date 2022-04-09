71699251deb396931085f068b8412913f9258b71
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-04-09 10:02:28 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  *main - Entry point
  *My hello, printf task
  *Return: return 1 after program execution
-*/
+ */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf ( "with proper grammar, but the outcome is a piece of art,\n" );
 return(0);
 }
