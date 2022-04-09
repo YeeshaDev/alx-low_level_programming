eab5347ee3e4090d458db02c04e15da47db15378
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-04-09 10:09:22 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,9 @@
  *Return: 0 after printing the function
  */
 int main(void)
-{ 
-puts("\ "Programming is like building a multilingual puzzle");
+{
+  
+puts("\"Programming is like building a multilingual puzzle");
+  
 return(0);
 }
