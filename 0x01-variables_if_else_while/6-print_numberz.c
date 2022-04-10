73b4844aaa3eb7c1825b2ae6961684857ccebe93
YeeshaDev <aishaagunbiade05@gmail.com>
2022-04-10 15:56:04 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; i < 57; i++)
+{
+printf(i);
+}
+
+printf('\n');
+
+return (0);
+
+}
