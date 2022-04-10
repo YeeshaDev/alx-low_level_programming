2eab4ec1581065e4574d3b16f8c9f6a7b698fedd
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-04-10 14:29:00 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,12 +21,12 @@ printf("%i is zero\n", n);
 
 else if (n > 0)
 {
-printf("%i is negative\n", n);
+printf("%i is positive\n", n);
 }
 
 else
 {
-printf("%i is positive\n", n);
+printf("%i is negative\n", n);
 }
 
 return (0);
