5c0654cb6ed7f997ab23a3036f527af76e489117
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-04-10 17:44:17 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char** argv)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+ write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
 return (1);
 }
