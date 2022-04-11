60601f0927118e7a32a7c919801c29e27c3893dd
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-04-10 17:21:55 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,10 +13,10 @@ for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-putchar ((num2 / 10) + '0');
 putchar ((num1 % 10) + '0');
-putchar (' ');
 putchar ((num1 / 10) + '0');
+putchar (' ');
+putchar ((num2 / 10) + '0');
 putchar ((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
