a6bf42d29db36d5005d200a053be2ca23434fcbf
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-05-30 19:43:31 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,13 +8,13 @@
  *
  * Return: number of bits.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int k)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int nbits;
 
-	for (nbits = 0; n || k; n >>= 1, k >>= 1)
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
-		if ((n & 1) != (k & 1))
+		if ((n & 1) != (m & 1))
 			nbits++;
 	}
 
